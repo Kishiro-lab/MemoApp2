eb0eb326c0d2eeca664246ea970e84de7c4716b4
kishiro <92235077+Kishiro-lab@users.noreply.github.com>
2023-10-18 15:42:07 +0900

    Markup memo List screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,60 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function Index(): JSX.Element {
   return (
-    <View>
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      {/* header */}
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+      {/* memo list */}
+      <View>
+        {/* memo Item */}
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月16日 13:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        {/* memo Item */}
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月16日 13:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        {/* memo Item */}
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023年10月16日 13:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+      {/* button */}
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
